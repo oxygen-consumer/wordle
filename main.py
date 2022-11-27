5ea6f435902ee660fc2b6eda8d6c6a98a7108381
Paul Gagauta <paulaesthetical@gmail.com>
2022-11-27 15:37:34 +0200

    Add command line argument to open GUI

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import time
+from Controller.gui_ui import GUI
 from Controller.cli_ui import CLI
 from Logic.game_logic import WordleServ
 from Repository.default_repo import DefaultRepo
 from Controller.bot_handler import BotHandler
+import argparse
 
 
 def main():
@@ -15,14 +17,24 @@ def main():
 
     print("Initializing game...")
     serv = WordleServ(repo)
-    # TODO: GUI, CLI should be used when --cli is passed
-    ui = CLI(serv, bot_handler)
+
+    parser = argparse.ArgumentParser(description="Wordle Bot")
+    parser.add_argument(
+        "--gui",
+        action="store_true",
+        dest='gui')
+    parsed_args = parser.parse_args()
+
+    if parsed_args.gui:
+        ui = GUI(serv, bot_handler)
+    else:
+        ui = CLI(serv, bot_handler)
 
     print("Finisihed loading... Running...")
     start_time = time.perf_counter()
     ui.run_ui()
     end_time = time.perf_counter()
-    print(f"Finished guessing all the words in {start_time - end_time}.")
+    print(f"Finished guessing all the words in {end_time - start_time}.")
 
 
 if __name__ == "__main__":
